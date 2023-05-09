7aaa500fc69977b61e0d26868e8728a7850df993
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-09 08:20:59 +0000

    Refector

--- .devcontainer/scripts/devspace-on-create.sh
+++ .devcontainer/scripts/devspace-on-create.sh
@@ -8,6 +8,15 @@ kind create cluster --config /home/vscode/.devcontainer/clusters/default.yaml
 kubectl config use-context kind-default
 kubectl version --short
 kubectl cluster-info
+# https://helm.sh/docs/intro/quickstart/
+# https://github.com/kubernetes/dashboard
+# https://github.com/imorti/kind-dashboard-setup
+# https://artifacthub.io/packages/helm/k8s-dashboard/kubernetes-dashboard
+# https://kubernetes.io/docs/tasks/access-application-cluster/web-ui-dashboard/
+# https://artifacthub.io/packages/helm/k8s-dashboard/kubernetes-dashboard
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard
+# https://medium.com/@munza/local-kubernetes-with-kind-helm-dashboard-41152e4b3b3d
 # https://kind.sigs.k8s.io/docs/user/ingress/#ingress-nginx
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 kubectl wait --namespace ingress-nginx \
@@ -21,6 +30,3 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 ~/scripts/create-kubernetes-devcert.sh argocd argocd-server-tls argocd-server-tls-password
 ~/scripts/create-kubernetes-devcert.sh argocd argocd-repo-server-tls argocd-repo-server-tls-password
 ~/scripts/create-kubernetes-devcert.sh argocd argocd-dex-server-tls argocd-dex-server-tls-password
-# https://github.com/kubernetes/dashboard
-# https://github.com/imorti/kind-dashboard-setup
-# https://helm.sh/docs/intro/quickstart/
