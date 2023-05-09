a8b52cb1788031636bf9ea7489500b553abfbc26
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-09 19:45:43 -0400

    Working on Scripts

--- src/scripts/devspace-on-create.sh
+++ src/scripts/devspace-on-create.sh
@@ -10,7 +10,7 @@ npm install -g @devcontainers/cli
 # https://cloud.google.com/anthos/clusters/docs/on-prem/latest/troubleshoot-user-cluster-create-api
 # https://itnext.io/kubernetes-kind-cheat-shee-2605da77984
 # existingCluster=$(kind get cluster default)
-kind create cluster --wait 30s --config /home/vscode/clusters/default.yaml
+kind create cluster --wait 30s --config src/clusters/default.yaml
 # https://kubernetes.io/docs/reference/generated/kubectl/kubectl-commands
 # https://kubernetes.io/docs/reference/kubectl/cheatsheet/
 # https://kubernetes.io/docs/reference/kubectl/
