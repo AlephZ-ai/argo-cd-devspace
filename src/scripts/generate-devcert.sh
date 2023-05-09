a9ce663cc2bf294fbe01475c7977dd8abcd6174c
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-09 19:20:31 -0400

    Working on Scripts

--- src/scripts/generate-devcert.sh
+++ src/scripts/generate-devcert.sh
@@ -5,10 +5,10 @@
 certpurpose=$1
 password=$2
 root=root
-mkdir -p ../../devcerts/$certpurpose
-openssl req -new -nodes -newkey rsa:2048 -keyout ../../devcerts/$certpurpose/cert.key -out ../../devcerts/$certpurpose/cert.csr -subj "/C=US/ST=NC/L=Rolesville/O=$certpurpose/CN=localhost.local"
-openssl x509 -req -sha256 -days 3650 -in ../../devcerts/$certpurpose/cert.csr -CA ../../devcerts/$root/cert.pem -CAkey ../../devcerts/$root/cert.key -CAcreateserial -extfile ../../devcerts/$root/domains.ext -out ../../devcerts/$certpurpose/cert.crt
+mkdir -p devcerts/$certpurpose
+openssl req -new -nodes -newkey rsa:2048 -keyout devcerts/$certpurpose/cert.key -out devcerts/$certpurpose/cert.csr -subj "/C=US/ST=NC/L=Rolesville/O=$certpurpose/CN=localhost.local"
+openssl x509 -req -sha256 -days 3650 -in devcerts/$certpurpose/cert.csr -CA devcerts/$root/cert.pem -CAkey devcerts/$root/cert.key -CAcreateserial -extfile devcerts/$root/domains.ext -out devcerts/$certpurpose/cert.crt
 # https://stackoverflow.com/questions/808669/convert-a-cert-pem-certificate-to-a-pfx-certificate
 # https://stackoverflow.com/questions/63441247/how-to-pass-pkcs12-password-into-openssl-conversion-module
-openssl pkcs12 -passout pass:$password -inkey ../../devcerts/$certpurpose/cert.key -in ../../devcerts/$certpurpose/cert.crt -export -out ../../devcerts/$certpurpose/cert.pfx
-git add -f ../../devcerts/$certpurpose/cert.pfx
+openssl pkcs12 -passout pass:$password -inkey devcerts/$certpurpose/cert.key -in devcerts/$certpurpose/cert.crt -export -out devcerts/$certpurpose/cert.pfx
+git add -f devcerts/$certpurpose/cert.pfx
