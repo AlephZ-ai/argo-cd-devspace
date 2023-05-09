19074cf9958fa4d5e2603c2653fb95e55bfd9522
Aaron Stainback <acehack00@gmail.com>
2023-05-09 14:06:08 -0400

    Working on certs

--- .devcontainer/scripts/generate-devcert.sh
+++ .devcontainer/scripts/generate-devcert.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-# TODO: Make sure this script is idempotent since it will be run multiple times
 # https://linuxhandbook.com/bash-arguments
 # https://support.microfocus.com/kb/doc.php?id=7013103
+# https://gist.github.com/cecilemuller/9492b848eb8fe46d462abeb26656c4f8
 certpurpose=$1
 password=$2
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout devcerts/$certpurpose/key.pem -out devcerts/$certpurpose/cert.pem
+subject=$3
+mkdir -p devcerts/$certpurpose
+openssl req -x509 -nodes -new -sha256 -days 3650 -newkey rsa:2048 -keyout devcerts/$certpurpose/cert.key -out devcerts/$certpurpose/cert.pem -subj $subject
+openssl x509 -outform pem -in devcerts/$certpurpose/cert.pem -out devcerts/$certpurpose/cert.crt
 # https://stackoverflow.com/questions/808669/convert-a-cert-pem-certificate-to-a-pfx-certificate
 # https://stackoverflow.com/questions/63441247/how-to-pass-pkcs12-password-into-openssl-conversion-module
-openssl pkcs12 -passout pass:$password -inkey devcerts/$certpurpose/key.pem -in devcerts/$certpurpose/cert.pem -export -out devcerts/$certpurpose/cert.pfx
+openssl pkcs12 -passout pass:$password -inkey devcerts/$certpurpose/cert.key -in devcerts/$certpurpose/cert.crt -export -out devcerts/$certpurpose/cert.pfx
+git add -f devcerts/$certpurpose/cert.pfx
---
+++ .devcontainer/scripts/generate-root-ca-devcert.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+root=root
+./scripts/generate-devcert.sh $root $root-password "/C=US/CN=Dev-Root-CA"
