c0557616cf06ee1bd77e02408341230118541ef4
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 01:54:45 -0400

    Working on Scripts

--- src/scripts/devspace-post-start.sh
+++ src/scripts/devspace-post-start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Make sure this script is idempotent since it will be run multiple times
+# https://code.visualstudio.com/remote/advancedcontainers/start-processes
 ./src/scripts/set-default-argo-password.sh
 # keep port forwards alive don't shut down after the process ends.
 tail -f /dev/null
