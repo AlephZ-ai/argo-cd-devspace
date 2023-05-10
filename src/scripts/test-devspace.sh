3a7be860bfe23493a3ada889934ab4791dee273d
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 22:00:12 +0000

    Fixing test

--- src/scripts/test-devspace.sh
+++ src/scripts/test-devspace.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 devcontainer up --workspace-folder .
-containerid=docker ps -q -f ancestor=cr.alephz.ai/devcontainers/kindest-argo-cd:latest
+containerid=$(docker ps -q -f name=kindest-argo-cd_devcontainer-kindest)
 docker rm -f $containerid -f
 docker container prune -f
 docker image prune -a -f
 docker network prune -f
-docker volume prune -a -f
+docker volume prune -f
