ecb1262a4b895e29988a91581a4d8cad69cb9ccd
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 06:06:17 +0000

    sleep infinity

--- src/scripts/devspace-post-start.sh
+++ src/scripts/devspace-post-start.sh
@@ -3,4 +3,4 @@
 # https://code.visualstudio.com/remote/advancedcontainers/start-processes
 ./src/scripts/set-default-argo-password.sh
 # keep port forwards alive don't shut down after the process ends.
-tail -f /dev/null
+sleep infinity
