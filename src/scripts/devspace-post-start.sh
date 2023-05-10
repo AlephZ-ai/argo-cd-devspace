73530e3bc94b4fd14db83166f44d533b8ccb865d
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 06:23:27 +0000

    idpaeoanf

--- src/scripts/devspace-post-start.sh
+++ src/scripts/devspace-post-start.sh
@@ -3,6 +3,6 @@
 # https://code.visualstudio.com/remote/advancedcontainers/start-processes
 ./src/scripts/set-default-argo-password.sh
 echo "Setup Complete Ctrl+Shift+~ to open a terminal"
-echo "Press Ctrl+Shift+~ to open a terminal"
+echo "Press Ctrl+Shift+~ to open a terminal in the current dev container"
 # keep port forwards alive don't shut down after the process ends.
 sleep infinity
--- src/scripts/forward-argo-ports.sh
+++ src/scripts/forward-argo-ports.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-# TODO: Make sure this script is idempotent since it will be run multiple times
 # https://kubernetes.io/docs/tasks/access-application-cluster/port-forward-access-application-cluster/
+# https://phoenixnap.com/kb/kubectl-port-forward
+existingForward=$(ps -ef | grep port-forward | grep 7443)
+if [ "$existingForward" != "" ]; then
+    echo "Forward already exists. Exiting."
+    exit 0
+fi
 kubectl port-forward svc/argocd-server -n argocd 7443:443 &
