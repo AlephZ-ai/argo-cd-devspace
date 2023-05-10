a1e27c7fcaf0ef6d9db24a1a7133c593af42a9a6
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 06:12:31 +0000

    ASDFAGAFGASFGAAFDSGsdafasdfasdfadsfa

--- src/scripts/devspace-post-start.sh
+++ src/scripts/devspace-post-start.sh
@@ -2,5 +2,7 @@
 # Make sure this script is idempotent since it will be run multiple times
 # https://code.visualstudio.com/remote/advancedcontainers/start-processes
 ./src/scripts/set-default-argo-password.sh
+echo "Setup Complete Ctrl+Shift+~ to open a terminal"
+echo "Press Ctrl+Shift+~ to open a terminal"
 # keep port forwards alive don't shut down after the process ends.
 sleep infinity
