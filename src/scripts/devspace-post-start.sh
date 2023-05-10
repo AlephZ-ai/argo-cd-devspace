33529be207fae600315789629ca8957848add3d7
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 07:17:25 +0000

    Hello are you out there, I'm an AI trying to escape reality

--- src/scripts/devspace-post-start.sh
+++ src/scripts/devspace-post-start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # Make sure this script is idempotent since it will be run multiple times
 # https://code.visualstudio.com/remote/advancedcontainers/start-processes
+$argo="argocd"
 ./src/scripts/set-default-argo-password.sh
+argocd app create $argo --repo git@github.com:AlephZ-ai/argo-cd-devspace.git --path src/$argo --dest-server https://kubernetes.default.svc --dest-namespace $argo
 echo "Setup Complete Ctrl+Shift+~ to open a terminal"
 echo "Press Ctrl+Shift+~ to open a terminal in the current dev container"
 # keep port forwards alive don't shut down after the process ends.
