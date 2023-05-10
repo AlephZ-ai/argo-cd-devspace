1efff718389fb6e9733b257f6d3f7c5ee864f882
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 05:43:08 +0000

    tail

--- src/scripts/devspace-attach.sh
+++ src/scripts/devspace-attach.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # Make sure this script is idempotent since it will be run multiple times
 ./src/scripts/set-default-argo-password.sh
+# keep port forwards alive don't shut down after the process ends.
+tail -f /dev/null
