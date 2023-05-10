2399f7861394eb31f0371fa57bf504e7b8563ef2
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 05:00:05 +0000

    idempotentency

--- src/scripts/forward-argo-ports.sh
+++ src/scripts/forward-argo-ports.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# TODO: Make sure this script is idempotent since it will be run multiple times
 # https://kubernetes.io/docs/tasks/access-application-cluster/port-forward-access-application-cluster/
 kubectl port-forward svc/argocd-server -n argocd 7443:443 &
