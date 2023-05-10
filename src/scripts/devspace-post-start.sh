3010ac09c1433a07d8cb37cb873351696ada8306
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 13:45:16 -0400

    Trying to hook up argo to github

--- src/scripts/devspace-post-start.sh
+++ src/scripts/devspace-post-start.sh
@@ -6,8 +6,8 @@ $argo="argocd"
 argocd cert add-tls $namespace.local --from ./devcerts/root/cert.crt
 # https://docs.github.com/en/codespaces/developing-in-codespaces/using-github-codespaces-with-github-cli#ssh-into-a-codespace
 # https://github.com/argoproj/argo-cd/blob/master/docs/user-guide/private-repositories.md
-argocd repo add git@github.com:AlephZ-ai/kindest-argo-cd.git --ssh-private-key-path ~/.ssh/id_rsa
-argocd app create $argo --repo git@github.com:AlephZ-ai/kindest-argo-cd.git --path src/$argo --dest-server https://kubernetes.default.svc --dest-namespace $argo --directory-recursegit a
+argocd repo add https://github.com/AlephZ-ai/kindest-argo-cd.git --username token --password $GITHUB_TOKEN
+argocd app create $argo --repo https://github.com/AlephZ-ai/kindest-argo-cd.git --path src/$argo --dest-server https://kubernetes.default.svc --dest-namespace $argo --directory-recursegit a
 echo "Setup Complete Ctrl+Shift+~ to open a terminal"
 echo "Press Ctrl+Shift+~ to open a terminal in the current dev container"
 # keep port forwards alive don't shut down after the process ends.
