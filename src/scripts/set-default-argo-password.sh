106c3bdb9a46a738d34d360fea29a5bbffeba17e
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 04:30:27 +0000

    Less chat'

--- src/scripts/set-default-argo-password.sh
+++ src/scripts/set-default-argo-password.sh
@@ -4,7 +4,7 @@ secret=$namespace-initial-admin-secret
 kubectl config set-context --current --namespace=$namespace
 echo "Waiting for $namespace password. CTRL-C to exit."
 while ! (kubectl get secret $secret 2>&1); do sleep 3; done
-kubectl port-forward svc/$namespace-server 7443:443 &
+kubectl port-forward svc/$namespace-server 7443:443 2>&1 &
 ./src/scripts/argo-login.sh
 defaultpassword=password
 currentpassword=$(kubectl get secret $secret -o jsonpath="{.data.password}" | base64 --decode)
