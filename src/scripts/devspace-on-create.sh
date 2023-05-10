644f6d5dd04d0581d7558974e783b8bb6744f105
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 19:59:23 +0000

    changes

--- src/scripts/devspace-on-create.sh
+++ src/scripts/devspace-on-create.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Make sure this script is idempotent since it will be run multiple times
+rm -f nohup.out
 sudo chsh -s $(which zsh) $(whoami)
 ./src/scripts/zsh-kubectl-autocomplete.sh
 # TODO: Why does this break things?
