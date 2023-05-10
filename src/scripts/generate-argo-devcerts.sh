75451cce95661a77d225f122b4e7ba0288ffdf79
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 09:57:35 +0000

    WTF

--- src/scripts/generate-argo-devcerts.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-argocdNamespace="argocd"
-src/scripts/generate-devcert.sh $argocdNamespace-server-tls $argocdNamespace-server-tls-password
-src/scripts/generate-devcert.sh $argocdNamespace-repo-server-tls $argocdNamespace-repo-server-tls-password
-src/scripts/generate-devcert.sh $argocdNamespace-dex-server-tls $argocdNamespace-dex-server-tls-password
--- src/scripts/install-argocd.sh
+++ src/scripts/install-argocd.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-# TODO: Make sure this script is idempotent since it will be run multiple times
 # https://argo-cd.readthedocs.io/en/stable/getting_started
 namespace="argocd"
+exists=$(kubectl get namespace argocd | grep argocd)
+if [ -n "$exists" ]; then
+  echo "$namespace already exists"
+  exit 0
+fi
 kubectl create namespace $namespace
 kubectl apply -n $namespace -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # https://argo-cd.readthedocs.io/en/stable/operator-manual/tls/
-./src/scripts/create-kubernetes-devcert.sh $namespace $namespace-server-tls
-./src/scripts/create-kubernetes-devcert.sh $namespace $namespace-repo-server-tls
-./src/scripts/create-kubernetes-devcert.sh $namespace $namespace-dex-server-tls
 # https://www.pcbaecker.com/posts/setup-argocd/
 # https://medium.com/devopsturkiye/self-managed-argo-cd-app-of-everything-a226eb100cf0
 # https://argo-cd.readthedocs.io/en/stable/getting_started/
