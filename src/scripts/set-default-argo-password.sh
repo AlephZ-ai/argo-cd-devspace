7ec3b63aa7ae3be967c8478674e3c8a9574cd7a2
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 03:26:39 +0000

    Scripts

--- src/scripts/set-default-argo-password.sh
+++ src/scripts/set-default-argo-password.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
-secret=argocd-initial-admin-secret
-echo "Waiting for argo password. CTRL-C to exit."
+namespace=argocd
+secret=$namespace-initial-admin-secret
+echo "Waiting for $namespace password. CTRL-C to exit."
 while ! (kubectl get secret $secret 2>&1); do sleep 3; done
 argo-login.sh
 defaultpassword=password
 currentpassword=$(kubectl get secret $secret -o jsonpath="{.data.password}" | base64 --decode)
 argocd account update-password --current-password $currentpassword --new-password $defaultpassword
-$encodedpassword=$(echo -n $argopassword | base64)
+$encodedpassword=$(echo -n $defaultpassword | base64)
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
 data:
   password: $encodedpassword
 kind: Secret
 metadata:
-  creationTimestamp: "2023-05-10T02:48:06Z"
-  name: argocd-initial-admin-secret
-  namespace: argocd
-  resourceVersion: "1078"
-  uid: 9a796a3e-fc4d-4251-891f-242f1c023fa9
+  name: $secret
+  namespace: $namespace
 type: Opaque
 EOF
 argo-login.sh
