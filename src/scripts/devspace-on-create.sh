e8591b964942fea75b2d746c5436d70ac914b16a
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 04:16:52 +0000

    Login

--- src/scripts/devspace-on-create.sh
+++ src/scripts/devspace-on-create.sh
@@ -8,13 +8,13 @@
 # https://artifacthub.io/packages/helm/k8s-dashboard/kubernetes-dashboard
 # https://kubernetes.io/docs/tasks/access-application-cluster/web-ui-dashboard/
 # https://artifacthub.io/packages/helm/k8s-dashboard/kubernetes-dashboard
-helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard
+# helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+# helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard
 # https://medium.com/@munza/local-kubernetes-with-kind-helm-dashboard-41152e4b3b3d
 # https://kind.sigs.k8s.io/docs/user/ingress/#ingress-nginx
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-kubectl wait --namespace ingress-nginx \
-  --for=condition=ready pod \
-  --selector=app.kubernetes.io/component=controller \
-  --timeout=90s
+# kubectl wait --namespace ingress-nginx \
+#   --for=condition=ready pod \
+#   --selector=app.kubernetes.io/component=controller \
+#   --timeout=90s
 ./src/scripts/install-argocd.sh
--- src/scripts/set-default-argo-password.sh
+++ src/scripts/set-default-argo-password.sh
@@ -2,14 +2,14 @@
 namespace=argocd
 secret=$namespace-initial-admin-secret
 kubectl config set-context --current --namespace=$namespace
-kubectl port-forward svc/$namespace-server 7443:443 &
 echo "Waiting for $namespace password. CTRL-C to exit."
 while ! (kubectl get secret $secret 2>&1); do sleep 3; done
+kubectl port-forward svc/$namespace-server 7443:443 &
 ./src/scripts/argo-login.sh
 defaultpassword=password
 currentpassword=$(kubectl get secret $secret -o jsonpath="{.data.password}" | base64 --decode)
 argocd account update-password --current-password $currentpassword --new-password $defaultpassword
-encodedpassword=$($defaultpassword | base64)
+encodedpassword=$(echo $defaultpassword | base64)
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
 data:
