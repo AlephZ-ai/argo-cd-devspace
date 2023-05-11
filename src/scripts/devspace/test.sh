99df812b89e5e49d9cfeb5346b97621a789178c4
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 23:27:29 -0400

    clean->up

--- src/scripts/devspace/test.sh
+++ src/scripts/devspace/test.sh
@@ -2,6 +2,5 @@
 ./src/scripts/utils/chmod-plus-x.sh
 ./src/scripts/devspace/build.sh
 containerid=$(docker ps -q -f name=kindest-argo-cd_devcontainer)
-docker rm -f "$containerid"
 devcontainer exec --container-id "$containerid" argocd version --short
 ./src/scripts/devspace/clean.sh
