bfd7b4df6497ba10528f67bc6b34e6395f008d80
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-11 00:21:20 -0400

    Adding external command

--- chmod-plus-x.ps1
+++ chmod-plus-x.ps1
@@ -3,4 +3,5 @@ git update-index --chmod=+x .\chmod-plus-x
 git update-index --chmod=+x .\build-devspace
 git update-index --chmod=+x .\clean-devspace
 git update-index --chmod=+x .\test-devspace
+git update-index --chmod=+x .\external-argo-version
 ./src/scripts/utils/chmod-plus-x.ps1
