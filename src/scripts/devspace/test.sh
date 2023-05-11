aaab958e68f27c054cf9a3a45aa776b681c51a67
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-11 01:31:23 -0400

    update tests

--- src/scripts/devspace/test.sh
+++ src/scripts/devspace/test.sh
@@ -2,5 +2,5 @@
 ./src/scripts/utils/chmod-plus-x.sh
 ./src/scripts/devspace/build.sh
 containerid=$(docker ps -q -f name=kindest-argo-cd-devspace)
-devcontainer exec --container-id "$containerid" argocd version --short
+devcontainer exec --container-id "$containerid" zsh -c "/workspaces/kindest-argo-cd/external-argo-version"
 ./src/scripts/devspace/clean.sh
