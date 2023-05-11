add2da3893b85b04574d2f9862a92d3bd7a1abaa
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-11 05:56:58 +0000

    fix forward

--- src/scripts/argo/forward-ports.sh
+++ src/scripts/argo/forward-ports.sh
@@ -10,8 +10,10 @@ kubectl wait \
    --timeout=30s
 existingForward=$(ps -ef | grep port-forward | grep 17443)
 if [ "$existingForward" != "" ]; then
-    echo "Forward already exists. Killing."
-    pkill kubectl -9
+    echo "Forward already exists. Skipping."
+    return 0
+    # echo "Forward already exists. Killing."
+    # pkill kubectl -9
 fi
 (kubectl port-forward svc/argocd-server 17443:443 2>&1) &
 sleep 1s
