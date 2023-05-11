5ce6a45ceb40bc0b1af6301e6908e9155b1ba48e
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 21:19:39 -0400

    Rename

--- chmod-plus-x.ps1
+++ chmod-plus-x.ps1
@@ -1,5 +1,5 @@
 git add .
 git update-index --chmod=+x .\chmod-plus-x
-git update-index --chmod=+x .\devspace-build
-git update-index --chmod=+x .\devspace-clean
+git update-index --chmod=+x .\build-devspace
+git update-index --chmod=+x .\clean-devspace
 ./src/scripts/utils/chmod-plus-x.ps1
