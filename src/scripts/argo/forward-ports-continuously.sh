d69242c4158fecc83873191b0519defcaa6c3682
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-11 18:30:51 -0400

    fixes

--- src/scripts/argo/forward-ports-continuously.sh
+++ src/scripts/argo/forward-ports-continuously.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+#shellcheck disable=SC2009
 existingForward=$(ps -ef | grep port-forward | grep "$$KINDEST_ARGO_CD_ARGO_PORT")
 if [ -n "$existingForward" ]; then
     echo "Forward already exists. Skipping."
     return 0
 fi
-while ./src/scripts/argo/forward-ports.sh; do sleep 5s; done
+while ./src/scripts/argo/forward-ports.sh; do sleep 1s; done
