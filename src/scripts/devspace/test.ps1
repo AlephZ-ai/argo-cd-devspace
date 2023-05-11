628ad8e8152ea6cf2895a06b6daf6da43cacb418
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-11 01:30:25 -0400

    update tests

--- src/scripts/devspace/test.ps1
+++ src/scripts/devspace/test.ps1
@@ -1,5 +1,5 @@
 ./src/scripts/utils/chmod-plus-x.ps1
 ./src/scripts/devspace/build.ps1
 $containerid = docker ps -q -f name=kindest-argo-cd-devspace
-devcontainer exec --container-id "$containerid" ./src/scripts/argo/forward-ports.sh && argocd version --short
+devcontainer exec --container-id "$containerid" zsh -c "/workspaces/kindest-argo-cd/external-argo-version"
 ./src/scripts/devspace/clean.ps1
