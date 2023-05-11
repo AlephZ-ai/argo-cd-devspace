ce087582ef50c70a974fb1d851213047822626ce
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-11 00:32:18 -0400

    fixing script

--- src/scripts/argo/forward-ports.sh
+++ src/scripts/argo/forward-ports.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+#shellcheck disable=SC2009
 # https://kubernetes.io/docs/tasks/access-application-cluster/port-forward-access-application-cluster/
 # https://phoenixnap.com/kb/kubectl-port-forward
-existingForward=$(ps -ef | pgrep port-forward | grep 17443)
+existingForward=$(ps -ef | grep port-forward | grep 17443)
 if [ "$existingForward" != "" ]; then
     echo "Forward already exists. Exiting."
     exit 0
 fi
 (kubectl port-forward svc/argocd-server 17443:443 2>&1) &
+sleep 1s
