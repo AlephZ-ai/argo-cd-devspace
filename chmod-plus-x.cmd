ffb20d76ab802f0954482a88cbc9363ae0dd1cd5
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 20:34:00 -0400

    Adding chmod shortcut

---
+++ chmod-plus-x.cmd
@@ -0,0 +1,1 @@
+powershell -command "chmod-plus-x.ps1"
---
+++ chmod-plus-x.ps1
@@ -0,0 +1,3 @@
+git update-index --chmod=+x .\chmod-plus-x
+git update-index --chmod=+x .\devspace-clean
+./src/scripts/utils/chmod-plus-x.ps1
