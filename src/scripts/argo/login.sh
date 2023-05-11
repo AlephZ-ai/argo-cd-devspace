5c1dee040774927c927640a8e590cb5d6993397a
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-10 22:52:19 -0400

    f

--- src/scripts/argo/login.sh
+++ src/scripts/argo/login.sh
@@ -3,4 +3,4 @@ secret=argocd-initial-admin-secret
 echo "Waiting for argo password. CTRL-C to exit."
 while ! (kubectl get secret $secret 2>&1); do sleep 10; done
 password=$(kubectl get secret $secret -o jsonpath="{.data.password}" | base64 --decode)
-argocd login --insecure localhost:7443 --username admin --password "$password"
+argocd login --insecure localhost:17443 --username admin --password "$password"
