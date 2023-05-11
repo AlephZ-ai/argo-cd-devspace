422c46253d7414702ca8bcc25bb11c90d8f2fb66
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-11 02:25:26 +0000

    completion change

--- src/scripts/devspace/zsh-kubectl-completion.sh
+++ src/scripts/devspace/zsh-kubectl-completion.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env zsh
 # shellcheck disable=SC1071
 # completions must use zsh since it's the default shell in the devcontainer
+source ~/.zshrc
+autoload -U +X compinit && compinit
 source <(kubectl completion zsh)
