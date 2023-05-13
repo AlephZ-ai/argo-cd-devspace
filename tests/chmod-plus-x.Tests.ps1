a3502978fd7e86ce2a363d4011886ab58fa06d7a
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-13 03:17:22 -0400

    fixing devcerts

---
+++ tests/chmod-plus-x.Tests.ps1
@@ -0,0 +1,22 @@
+BeforeAll {
+    $script = $PSCommandPath | Resolve-Path -Relative
+    Write-Host "${script}: Starting."
+    & "$($PSCommandPath | Resolve-Path -Relative:$false | Split-Path -Parent | Split-Path -Parent)/src/scripts/devspace/setup/set-env-vars.ps1"
+}
+
+AfterAll {
+    Write-Host "${script}: Finished."
+}
+
+Describe "chmod-plus-x.ps1" {
+    It 'Given no parameters, it should chmod +x all .sh files in the project recursively without errors' {
+        # Arrange
+        $scriptPath = Join-Path -Path ($PSScriptRoot | Resolve-Path -Relative:$false | Split-Path -Parent) -ChildPath "chmod-plus-x.ps1"
+
+        # Act
+        $result = { & $scriptPath }
+
+        # Assert
+        $result | Should -Not -Throw
+    }
+}
--- tests/generate-devcerts.Tests.ps1
+++ tests/generate-devcerts.Tests.ps1
@@ -1,7 +1,7 @@
 BeforeAll {
     $script = $PSCommandPath | Resolve-Path -Relative
     Write-Host "${script}: Starting."
-    & "$($PSCommandPath | Resolve-Path -Relative:$false | Split-Path -Parent)/src/scripts/devspace/setup/set-env-vars.ps1"
+    & "$($PSCommandPath | Resolve-Path -Relative:$false | Split-Path -Parent | Split-Path -Parent)/src/scripts/devspace/setup/set-env-vars.ps1"
 }
 
 AfterAll {
@@ -11,7 +11,7 @@ AfterAll {
 Describe "chmod-plus-x.ps1" {
     It 'Given no parameters, it should chmod +x all .sh files in the project recursively without errors' {
         # Arrange
-        $scriptPath = Join-Path -Path ($PSScriptRoot | Resolve-Path -Relative:$false) -ChildPath "chmod-plus-x.ps1"
+        $scriptPath = Join-Path -Path ($PSScriptRoot | Resolve-Path -Relative:$false | Split-Path -Parent) -ChildPath "generate-devcerts.ps1"
 
         # Act
         $result = { & $scriptPath }
