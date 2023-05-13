25c4c59a82178c3afb394b83312c068381660766
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-13 04:02:09 -0400

    Dont

--- clean-devspace.cmd
+++ clean-devspace.cmd
@@ -4,7 +4,6 @@ set script=%0
 set scriptFolder=%~dp0
 echo %script%: Starting.
 rem https://stackoverflow.com/questions/74862849/powershell-convertto-securestring-not-recognised-if-run-script-inline-from-cmd
-set "PSModulePath="
 set PROJECT_ROOT=%scriptFolder%
 PowerShell -file "%PROJECT_ROOT%/clean-devspace.ps1"
 echo %script%: Finished.
