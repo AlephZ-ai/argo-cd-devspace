a3471e5de2ebaf9ecf99977f91d98a8240ff5bb0
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-12 21:40:51 -0400

    fixing issues

--- generate-devcerts.cmd
+++ generate-devcerts.cmd
@@ -2,9 +2,9 @@
 setlocal
 set script=%0
 set scriptFolder=%~dp0
-echo "%script%: Starting."
-set SCRIPTS_ROOT="%scriptFolder%/src/scripts"
-pshell -file "%SCRIPTS_ROOT%/openssl/generate-root-ca.ps1"
-pshell -file "%SCRIPTS_ROOT%/argocd/generate-devcerts.ps1"
-echo "%script%: Finished."
+echo %script%: Starting.
+set SCRIPTS_ROOT=%scriptFolder%/src/scripts
+%pshell% -file "%SCRIPTS_ROOT%/openssl/generate-root-ca.ps1"
+%pshell% -file "%SCRIPTS_ROOT%/argocd/generate-devcerts.ps1"
+echo %script%: Finished.
 endlocal
---
+++ generate-devcerts.ps1
@@ -0,0 +1,11 @@
+$script = $PSCommandPath | Resolve-Path -Relative
+Write-Host "${script}: Starting."
+try {
+    & "$($PSCommandPath | Resolve-Path -Relative:$false | Split-Path -Parent)/src/scripts/devspace/setup/set-env-vars.ps1"
+    & "$env:PROJECT_ROOT/generate-devcerts.cmd"
+} catch [System.Exception] {
+    Write-Error "${script}: Error: $_"
+    exit 1
+} finally {
+   Write-Host "${script}: Finished."
+}
