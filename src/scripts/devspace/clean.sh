539d21c28f03ec1af682db34f674d149ccbf0616
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-13 14:54:24 -0400

    path fixes

--- src/scripts/devspace/clean.sh
+++ src/scripts/devspace/clean.sh
@@ -2,7 +2,7 @@
 #shellcheck disable=SC2034
 script=$(basename "$0")
 echo "$script: Starting."
-"$(dirname "$(dirname "$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)")")/devspace/setup/set-env-vars.sh"
+"$(dirname "$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)")/devspace/setup/set-env-vars.sh"
 for i in {1..3}
 do
     containerid=$(docker ps -q -f name="$PROJECT_NAME-devspace")
