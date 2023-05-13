8a5c16c2782bdb093c0f701c6afd002a84e035b9
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-13 15:11:34 -0400

    wait script fix

--- src/scripts/argocd/wait-for-password.sh
+++ src/scripts/argocd/wait-for-password.sh
@@ -3,5 +3,5 @@ script=$(basename "$0")
 echo "$script: Starting."
 "$(dirname "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)")/devspace/setup/set-env-vars.sh"
 secret=argocd-initial-admin-secret
-"$SCRIPTS_ROOT/k8s/wait-for-secret.sh $secret"
+"$SCRIPTS_ROOT/k8s/wait-for-secret.sh" "$secret"
 echo "$script: Finished."
--- src/scripts/k8s/wait-for-secret.sh
+++ src/scripts/k8s/wait-for-secret.sh
@@ -3,6 +3,6 @@ script=$(basename "$0")
 echo "$script: Starting."
 "$(dirname "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)")/devspace/setup/set-env-vars.sh"
 secret=$1
-echo "Waiting for argo password. CTRL-C to exit."
+echo "Waiting for secret: $secret password. CTRL-C to exit."
 while ! (kubectl get secret "$secret" 2>&1); do sleep 10s; done
 echo "$script: Finished."
