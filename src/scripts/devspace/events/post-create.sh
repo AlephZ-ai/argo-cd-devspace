dc796565b221521b3e574519df04e0f5abf18dda
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-13 03:45:22 -0400

    script

--- src/scripts/devspace/events/post-create.sh
+++ src/scripts/devspace/events/post-create.sh
@@ -2,7 +2,7 @@
 # Make sure this script is idempotent since it will be run multiple times
 script=$(basename "$0")
 echo "$script: Starting."
-"$(dirname "$(dirname "$(dirname "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)")")")/devspace/setup/set-env-vars.sh"
+"$(dirname "$(dirname "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)")")/devspace/setup/set-env-vars.sh"
 "$SCRIPTS_ROOT/dotnet/configure-credential-manager.sh"
 "$SCRIPTS_ROOT/argocd/self-manage.sh"
 echo "Press Ctrl+Shift+~ to open a terminal in the current dev container"
---
+++ tests/up-devspace.Tests.ps1
@@ -0,0 +1,22 @@
+BeforeAll {
+    $script = $PSCommandPath | Resolve-Path -Relative
+    Write-Host "${script}: Starting."
+    & "$($PSCommandPath | Resolve-Path -Relative:$false | Split-Path -Parent | Split-Path -Parent)/src/scripts/devspace/setup/set-env-vars.ps1"
+}
+
+AfterAll {
+    Write-Host "${script}: Finished."
+}
+
+Describe "build-devspace.ps1" {
+    It "Given no parameters, build the devspace without errors" {
+        # Arrange
+        $scriptPath = Join-Path -Path ($PSScriptRoot | Resolve-Path -Relative:$false | Split-Path -Parent) -ChildPath "up-devspace.ps1"
+
+        # Act
+        $result = { & $scriptPath }
+
+        # Assert
+        $result | Should -Not -Throw
+    }
+}
