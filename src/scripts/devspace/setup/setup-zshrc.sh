8cc9c48466e128241aeec546cec8e54d5700e779
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-14 13:11:55 -0400

    kubefirst

--- src/scripts/devspace/setup/setup-zshrc.sh
+++ src/scripts/devspace/setup/setup-zshrc.sh
@@ -5,5 +5,7 @@ grep -qxF "$source"  ~/.zshrc || echo "$source" >>  ~/.zshrc
 # TODO: Make sure this does not get inserted multiple times
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/vscode/.zprofile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sude mkdir -p ~/.ssh/
+chown vscode:vscode ~/.ssh/
 sudo touch ~/.ssh/known_hosts
 sudo bash -c eval "$(ssh-keyscan github.com >> ~/.ssh/known_hosts)"
