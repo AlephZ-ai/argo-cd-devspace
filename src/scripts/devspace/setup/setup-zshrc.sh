ae151cb9e37077f25788e9c7724a2772c285b74c
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-14 12:56:10 -0400

    kubefirst

--- src/scripts/devspace/setup/setup-zshrc.sh
+++ src/scripts/devspace/setup/setup-zshrc.sh
@@ -5,3 +5,5 @@ grep -qxF "$source"  ~/.zshrc || echo "$source" >>  ~/.zshrc
 # TODO: Make sure this does not get inserted multiple times
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/vscode/.zprofile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo touch ~/.ssh/known_hosts
+sudo sh -c eval "$(ssh-keyscan github.com >> ~/.ssh/known_hosts)"
--- src/scripts/git/install-git-credential-manager.sh
+++ src/scripts/git/install-git-credential-manager.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 "$KINDEST_ARGO_CD_SCRIPTS_ROOT/dotnet/install-tool.sh" git-credential-manager
-eval "$(ssh-keyscan github.com >> ~/.ssh/known_hosts)"
