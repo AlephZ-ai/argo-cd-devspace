435e57a931446560759d87098561a37fca8e5715
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-13 22:17:59 -0400

    refactor kind script

--- src/scripts/kind/create-kindest-cluster.sh
+++ src/scripts/kind/create-kindest-cluster.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
-script="$(basename "$0")"
-scriptPath="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-scriptsPath="$(dirname "$scriptPath")"
-script="$scriptPath/$script"
-echo "$script: Starting."
-"$scriptsPath/devspace/setup/set-env-vars.sh"
+i="$0"
+timeout=30s
+echo "$i: Started creating kindest cluster."
 # https://kind.sigs.k8s.io/docs/user/quick-start/
 # https://cloud.google.com/anthos/clusters/docs/on-prem/latest/troubleshoot-user-cluster-create-api
 # https://itnext.io/kubernetes-kind-cheat-shee-2605da77984
-while ! (kind create cluster --wait 30s --config "$KINDEST_ARGO_CD_PROJECT_ROOT/src/clusters/$KINDEST_ARGO_CD_CLUSTER_NAME.yaml" 2>&1); do kind delete clusters "$KINDEST_ARGO_CD_CLUSTER_NAME"; done
+while ! (kind create cluster --wait "$timeout" --config "$KINDEST_ARGO_CD_PROJECT_ROOT/src/clusters/$KINDEST_ARGO_CD_CLUSTER_NAME.yaml" 2>/dev/null); do kind delete clusters "$KINDEST_ARGO_CD_CLUSTER_NAME"; done
 # https://kubernetes.io/docs/reference/generated/kubectl/kubectl-commands
 # https://kubernetes.io/docs/reference/kubectl/cheatsheet/
 # https://kubernetes.io/docs/reference/kubectl/
 kubectl config use-context "kind-$KINDEST_ARGO_CD_CLUSTER_NAME"
 kubectl version --short
 kubectl cluster-info
-echo "$script: Finished."
+echo "$i: Finished creating kindest cluster."
