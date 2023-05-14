692a2ff08ff8f9ae92b9ff124670924715b5ab24
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-14 15:56:51 -0400

    kubefirst

--- src/scripts/devspace/setup/create.sh
+++ src/scripts/devspace/setup/create.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 #shellcheck disable=2016
 rm -f nohup.out
+while ! (bash -c "NONINTERACTIVE=true && $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"); do echo "Retrying Homebrew Install"; sleep 1s; done
+"$KINDEST_ARGO_CD_SCRIPTS_ROOT/mkcert/install.sh"
 mkdir -p ~/.ssh/
 touch ~/.ssh/known_hosts
-bash -c eval "$(ssh-keyscan github.com >> ~/.ssh/known_hosts)"
 export PATH=$PATH:~/.dotnet/tools
+bash -c eval "$(ssh-keyscan github.com >> ~/.ssh/known_hosts)"
 dotnet dev-certs https --trust
 "$KINDEST_ARGO_CD_SCRIPTS_ROOT/git/install-git-credential-manager.sh"
 gh auth setup-git
-"$KINDEST_ARGO_CD_SCRIPTS_ROOT/mkcert/install.sh"
-while ! (bash -c "NONINTERACTIVE=true && $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"); do echo "Retrying Homebrew Install"; sleep 1s; done
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/vscode/.zprofile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 "$KINDEST_ARGO_CD_SCRIPTS_ROOT/devspace/setup/setup-zshrc.sh"
