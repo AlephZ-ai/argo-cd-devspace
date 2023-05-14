e299bb8c32962a151777de058d7452f864159b17
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-13 22:24:58 -0400

    refactor npm and pwsh script

--- src/scripts/npm/install.sh
+++ src/scripts/npm/install.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
-# https://docs.npmjs.com/getting-started/
-script="$(basename "$0")"
-scriptPath="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-scriptsPath="$(dirname "$scriptPath")"
-script="$scriptPath/$script"
-echo "$script: Starting."
-"$scriptsPath/devspace/setup/set-env-vars.sh"
-package=$1
+i="$0"
+package="$1"
+echo "$i: Starting installing node package: $package."
 npm update -g npm
 # https://stackoverflow.com/questions/33553082/how-can-i-update-all-npm-packages-modules-at-once
 npm i -g npm-check-updates && ncu -u && npm i
 npm install -g "$package"
-echo "$script: Finished."
+echo "$i: Finished installing node package: $package."
---
+++ src/scripts/pwsh/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+i="$0"
+module="$1"
+echo "$i: Started installing PowerShell module: $module."
+pshell -command Install-Module "$module" -Force -AcceptLicense    
+echo "$i: Finished installing PowerShell module: $module."
