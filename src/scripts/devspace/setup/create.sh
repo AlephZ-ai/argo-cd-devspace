22f83f8bec8a5c7aa9da3df3223cd8a68ebfbcdd
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-14 18:29:39 -0400

    kubefirst

--- src/scripts/devspace/setup/create.sh
+++ src/scripts/devspace/setup/create.sh
@@ -12,7 +12,6 @@ bash -c eval "$(ssh-keyscan github.com >> ~/.ssh/known_hosts)"
 dotnet dev-certs https --trust
 "$KINDEST_ARGO_CD_SCRIPTS_ROOT/git/install-git-credential-manager.sh"
 gh auth auth login
-gh auth setup-git
 # https://github.com/sigstore/gitsign/blob/main/cmd/gitsign-credential-cache/README.md
 while (gh auth refresh); echo "\$GITHUB_TOKEN refreshed"; do echo "caching \$GITHUBTOKEN"; gitsign-credential-cache; done &
 # TODO: Why does this break things?
--- src/scripts/git/install-git-credential-manager.sh
+++ src/scripts/git/install-git-credential-manager.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env zsh
 #shellcheck shell=bash
 "$KINDEST_ARGO_CD_SCRIPTS_ROOT/dotnet/install-tool.sh" git-credential-manager
-"$KINDEST_ARGO_CD_SCRIPTS_ROOT/git/configure-credential-manager.sh"
