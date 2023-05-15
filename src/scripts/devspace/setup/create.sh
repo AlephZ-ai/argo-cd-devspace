e08028123d7d84e1ce0b39463bd7cb8d816bba82
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-14 20:53:47 -0400

    kubefirst

--- src/scripts/devspace/setup/create.sh
+++ src/scripts/devspace/setup/create.sh
@@ -11,6 +11,7 @@ bash -c eval "$(ssh-keyscan github.com >> ~/.ssh/known_hosts)"
 dotnet dev-certs https --trust
 tool=git-credential-manager && if ! (dotnet tool install -g "$tool"); then dotnet tool update -g "$tool"; fi
 git-credential-manager configure
+git-credential-manager diagnose
 # https://github.com/sigstore/gitsign/blob/main/cmd/gitsign-credential-cache/README.md
 brew update
 brew upgrade
