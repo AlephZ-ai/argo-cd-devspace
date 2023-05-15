f3454271c93244ae96cb99ada5e1fe301a7ca8d0
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-15 09:18:21 -0400

    kubefirst

--- src/scripts/devspace/setup/create.sh
+++ src/scripts/devspace/setup/create.sh
@@ -13,6 +13,7 @@ dotnet dev-certs https --trust
 tool=git-credential-manager && if ! (dotnet tool install -g "$tool"); then dotnet tool update -g "$tool"; fi
 git-credential-manager configure
 git-credential-manager diagnose
+gh auth login --with-token <<< "$ALEPHZ_AI_ADMIN_GITHUB_TOKEN"
 # https://github.com/sigstore/gitsign/blob/main/cmd/gitsign-credential-cache/README.md
 brew update
 brew upgrade
@@ -25,7 +26,7 @@ module=Set-PsEnv && pwsh -command Install-Module "$module" -Force -AcceptLicense
 module=Pester && pwsh -command Install-Module "$module" -Force -AcceptLicense
 brew=kubefirst/tools/kubefirst && brew install "$brew"
 "$KINDEST_ARGO_CD_SCRIPTS_ROOT/devspace/setup/set-env-vars.sh"
-pushd /tmp
+pushd /tmp || exit
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt install --fix-broken -y
@@ -35,7 +36,7 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
 sudo rm microsoft.gpg
-popd
-while ! (bash -c "kubefirst k3d create"); do echo "Retrying Create Kubefirst Cluster"; sleep 1s; done
+popd || exit
+while ! (bash -c "kubefirst k3d create "); do echo "Retrying Create Kubefirst Cluster"; sleep 1s; done
 kubefirst k3d root-credentials
 kubectl get pods -A
