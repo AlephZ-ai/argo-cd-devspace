ae4843a8488814fa527ea0eac4faee2877a0795a
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-15 12:13:59 -0400

    fixing port issue

--- src/scripts/devspace/setup/init.sh
+++ src/scripts/devspace/setup/init.sh
@@ -6,6 +6,6 @@ if ! (gh auth status); then gh auth login; fi
 while echo "gitsign credentials cached"; gitsign-credential-cache | true; do true; done &
 export GITSIGN_CREDENTIAL_CACHE="$HOME/.cache/sigstore/gitsign/cache.sock"
 Run Kubefirst
-while ! (bash -c "kubefirst local create "); do echo "Retrying Create Kubefirst Cluster"; sleep 1s; done
+while ! (bash -c "kubefirst local create"); do echo "Retrying Create Kubefirst Cluster"; sleep 1s; done
 kubefirst k3d root-credentials
 kubectl get pods -A
