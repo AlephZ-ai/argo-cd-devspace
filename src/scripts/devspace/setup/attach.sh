7ff516d970988f91069f7d36b9f9b802afb0e51a
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-15 10:44:27 -0400

    cleanup

--- src/scripts/devspace/setup/attach.sh
+++ src/scripts/devspace/setup/attach.sh
@@ -2,6 +2,5 @@
 #shellcheck shell=bash
 # "$KINDEST_ARGO_CD_SCRIPTS_ROOT/argocd/forward-port-continuously.sh"
 # "$KINDEST_ARGO_CD_SCRIPTS_ROOT/argocd/login.sh"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 # Setup Zsh profile
 "$KINDEST_ARGO_CD_SCRIPTS_ROOT/devspace/setup/setup-zshrc.sh"
--- src/scripts/devspace/setup/init.sh
+++ src/scripts/devspace/setup/init.sh
@@ -2,5 +2,4 @@
 #shellcheck shell=bash
 # "$KINDEST_ARGO_CD_SCRIPTS_ROOT/argocd/self-manage.sh"
 # Setup Zsh profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 "$KINDEST_ARGO_CD_SCRIPTS_ROOT/devspace/setup/setup-zshrc.sh"
