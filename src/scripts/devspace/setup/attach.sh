0a018124dec889564462af10dd67f749cf04f7f4
Aaron Stainback <aaron_bond@yahoo.com>
2023-05-15 14:51:46 -0400

    dev container fixes

--- src/scripts/devspace/setup/attach.sh
+++ src/scripts/devspace/setup/attach.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env zsh
 #shellcheck shell=bash
-# Cleanup log from previous attach
-rm -f nohup.out
 # Stand cluster back up in background process
 while ! kubefirst local create; do sleep 1s; done
 kubefirst k3d root-credentials
--- src/scripts/devspace/setup/init.sh
+++ src/scripts/devspace/setup/init.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env zsh
 #shellcheck shell=bash
 # Run Kubefirst
+# Cleanup previous logs
+rm -f nohup.out
